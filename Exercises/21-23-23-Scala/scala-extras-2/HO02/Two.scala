35e6a3832538c91968d6b5d0968d22a29d71dc77
Atin <atingupta2005@gmail.com>
2021-05-09 01:34:11 +0000

    -

--- Exercises/21-23-23-Scala/scala-extras-2/HO02/Two.scala
+++ Exercises/21-23-23-Scala/scala-extras-2/HO02/Two.scala
@@ -3,7 +3,7 @@ package ho2
 object Two extends App {
 
    def checkIF10AndReturn20(x: Int): Int =  {
-      if(x == 10)
+      if(x >= 10)
         x * 2
       else
         throw new Exception("Sorry, Value wasn't 10")
